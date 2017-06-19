bc1bbd055e7fba09d976ae13fec9180b34aaa2bb
Carlos C Soto <eclipxe13@gmail.com>
2017-06-19 18:32:32 -0500

    Update new docker images based on debian:stretch

--- docker/root/usr/lib/firefox/defaults/pref/local-settings.js
+++
@@ -1,3 +0,0 @@
-// You must start the file with a comment!
-pref("general.config.obscure_value", 0); // only needed if you do not want to obscure the content with ROT-13
-pref("general.config.filename", "mozilla.cfg");
